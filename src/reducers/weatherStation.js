a0c1462c87904084408e395f6a95a8347f554bc0
Sudheer Pal <sudheerpal2@gmail.com>
2018-09-27 04:35:28 +0530

    mapping action to reducer

--- src/reducers/weatherStation.js
+++ src/reducers/weatherStation.js
@@ -1,22 +1,28 @@
-
 import { FETCH_DATA_FULFILLED, FETCH_DATA_REJECTED } from "../constants/ActionTypes";
-import { APP_ID } from "../constants/generalConstants";
 
-import axios from "axios";
+export default function reducer(state = {
+  data: null,
+  status: null
+}, action) {
+  switch (action.type) {
+    case FETCH_DATA_FULFILLED: {
+      return {
+        ...state,
+        data: action.payload,
+        status: "success"
+      };
+      break;
+    }
+    case FETCH_DATA_REJECTED: {
+      return {
+        ...state,
+        status: "failed"
+      };
 
-export const fetchData = (region) => (dispatch) => {
-const { latitude, longitude } = region || {};
+      console.error(`Could not fetch the data from webservice. ${action.payload}.`); // eslint-disable-line
+      break;
+    }
+  }
 
-const getDataByCity = `https://api.openweathermap.org/data/2.5/forecast?q=${region}&units=metric&appid=${APP_ID}`;
-const getDataByCoords = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${APP_ID}`;
-
-let location = typeof(region) === "object" ? getDataByCoords : getDataByCity;
-
-return axios.get(location)
-.then((response) => {
-dispatch({type: FETCH_DATA_FULFILLED, payload: response.data});
-})
-.catch((err) => {
-dispatch({type: FETCH_DATA_REJECTED, payload: err}); // Error handling
-});
-};
+  return state;
+}
